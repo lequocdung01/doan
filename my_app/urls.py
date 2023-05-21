4d40aeaa6919ff120e22e97f162551174211822d
Dung <20521208@gm.uit.edu.vn>
2023-05-21 18:25:44 +0700

    giao dien 1

--- my_app/urls.py
+++ my_app/urls.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.get_my_app)
+    path('',views.get_my_app,name='home'),
+    path('detail/',views.detail, name='detail'),
+    path('cart/',views.cart, name='cart'),
 ]
