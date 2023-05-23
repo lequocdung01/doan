ed4fcb612a6291998240f6f159743c2b101893e0
Dung <20521208@gm.uit.edu.vn>
2023-05-23 19:34:04 +0700

    Dũng đã đăng tải

--- my_app/views.py
+++ my_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from .models import *
+from django.http import HttpResponse
 # Create your views here.
 def get_my_app(request):
     product = Product.objects.all()
     context = {'product': product}
-    return render(request,'home.html',context)
+    return render(request,'html/home.html',context)
 
 def detail(request):
     context={}
@@ -14,3 +15,14 @@ def cart(request):
     context={}
     return render(request, 'html/cart.html', context)
 
+def cart(request):
+    context={}
+    return render(request, 'html/cart.html', context)
+
+def product_mypham(request):
+    context = {}
+    return render(request, 'html/product.html', context)
+
+def event(request):
+    context = {}
+    return render(request, 'html/event.html', context)
