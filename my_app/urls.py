e903a8ec7fe8f224350d45755f5c93afbc906bb6
Dung <20521208@gm.uit.edu.vn>
2023-05-26 10:59:08 +0700

    dung sua lan 4

--- my_app/urls.py
+++ my_app/urls.py
@@ -7,4 +7,11 @@ urlpatterns = [
     path('cart/',views.cart, name='cart'),
     path('mypham/',views.product_mypham, name='mypham'),
     path('event/',views.event, name='event'),
+    path('contactus/',views.contactus, name='contactus'),
+    path('regestermember/',views.regestermember, name='regestermember'),
+    path('memberkhuyenmai/',views.memberkhuyenmai, name='memberkhuyenmai'),
+    path('introduce/',views.introduce, name='introduce'),
+    path('TuyenDung/',views.TuyenDung, name='TuyenDung'),
+    path('location/',views.location, name='location'),
+    path('form/',views.form, name='form'),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -15,10 +15,6 @@ def cart(request):
     context={}
     return render(request, 'html/cart.html', context)
 
-def cart(request):
-    context={}
-    return render(request, 'html/cart.html', context)
-
 def product_mypham(request):
     context = {}
     return render(request, 'html/category.html', context)
@@ -26,3 +22,31 @@ def product_mypham(request):
 def event(request):
     context = {}
     return render(request, 'html/event.html', context)
+
+def contactus(request):
+    context = {}
+    return render(request, 'html/contactus.html', context)
+
+def regestermember(request):
+    context = {}
+    return render(request, 'html/regestermember.html', context)
+
+def memberkhuyenmai(request):
+    context = {}
+    return render(request, 'html/memberkhuyenmai.html', context)
+
+def introduce(request):
+    context = {}
+    return render(request, 'html/introduce.html', context)
+
+def TuyenDung(request):
+    context = {}
+    return render(request, 'html/TuyenDung.html', context)
+
+def location(request):
+    context = {}
+    return render(request, 'html/location.html', context)
+
+def form(request):
+    context = {}
+    return render(request, 'html/form.html', context)
